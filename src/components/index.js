2a69744dfd725067637170e0d457dcc79532f0b6
HovsepStepanian <hovsepstepanian@gmail.com>
2022-09-09 23:43:49 +0300

    first commit

---
+++ src/components/index.js
@@ -0,0 +1,23 @@
+import Billing from "./Billing";
+import Business from "./Business.jsx";
+import CardDeal from "./CardDeal.jsx";
+import Clients from "./Clients.jsx";
+import CTA from "./CTA.jsx";
+import Footer from "./Footer.jsx";
+import Hero from "./Hero.jsx";
+import Navbar from "./Navbar.jsx";
+import Stats from "./Stats.jsx";
+import Testimonials from "./Testimonials.jsx";
+
+export {
+  Navbar,
+  Billing,
+  CardDeal,
+  Business,
+  Clients,
+  CTA,
+  Stats,
+  Footer,
+  Testimonials,
+  Hero,
+};
